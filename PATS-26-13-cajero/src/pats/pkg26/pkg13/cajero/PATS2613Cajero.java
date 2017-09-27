480a14777e72b708e28c5552ca43d7145a5403f8
Edgardo Torres Vanden Bouch <edgardo.torres@hotmail.com>
2017-09-27 18:51:14 -0500

    archivos base

---
+++ PATS-26-13-cajero/src/pats/pkg26/pkg13/cajero/PATS2613Cajero.java
@@ -0,0 +1,21 @@
+/*
+Ejercicio 13: Hacer un programa que simule un cajero automático con un saldo
+inicial de 1000 dolares, con el siguiente menú de opciones: 1.- Ingresar dinero
+a la cuenta. 2.- Retirar dinero de la cuenta. 3.- Salir
+*/
+package pats.pkg26.pkg13.cajero;
+
+/**
+ *
+ * @author edgardo
+ */
+public class PATS2613Cajero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
