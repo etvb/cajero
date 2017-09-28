50495f093b9fb60653452a20da2aa8cece4674a4
Edgardo Torres Vanden Bouch <edgardo.torres@hotmail.com>
2017-09-27 19:38:22 -0500

    resolvemos el problema con switch e if

--- PATS-26-13-cajero/src/pats/pkg26/pkg13/cajero/PATS2613Cajero.java
+++ PATS-26-13-cajero/src/pats/pkg26/pkg13/cajero/PATS2613Cajero.java
@@ -5,6 +5,8 @@ a la cuenta. 2.- Retirar dinero de la cuenta. 3.- Salir
 */
 package pats.pkg26.pkg13.cajero;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author edgardo
@@ -16,6 +18,39 @@ public class PATS2613Cajero {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        float SALDO_INICIAL = 1000;
+        float saldoActual, ingreso, retiro;
+        byte opcion;
+        
+        opcion = Byte.parseByte(JOptionPane.showInputDialog("Realiza una opcio\n"
+        + " 1.- Ingresar\n"+
+        "2.- Retirar\n" +
+        "3.- Salir"));
+        
+        switch(opcion){
+            case 1:
+                ingreso = Float.parseFloat(JOptionPane.showInputDialog("Cantidad a INGRESAR"));
+                saldoActual = ingreso + SALDO_INICIAL;
+                JOptionPane.showMessageDialog(null, "Su saldo es de: " + saldoActual);
+                break;
+            case 2: 
+                retiro  = Float.parseFloat(JOptionPane.showInputDialog("Cantidad a RETIRAR"));
+                if (retiro > SALDO_INICIAL ) {
+                    JOptionPane.showMessageDialog(null, "Saldo INSUFICIENTE");
+                }else{
+                saldoActual = SALDO_INICIAL - retiro; 
+                JOptionPane.showMessageDialog(null, "Su saldo es: " + saldoActual);
+                break;
+                }
+            case 3:
+                break;
+            default :
+                JOptionPane.showMessageDialog(null, "No existe la opcion");
+                break;
+        }
+       
+                
     }
     
 }
